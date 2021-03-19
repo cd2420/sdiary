457b498516070c85d82a2b6c36dc7138c1b2a782
임대근 <imdaegeun@imdaegeun-ui-MacBookPro.local>
2021-03-20 06:18:13 +0900

    v-1.4

--- src/config/common.js
+++ src/config/common.js
@@ -23,6 +23,7 @@ export const useStyles = makeStyles((theme) => ({
     formControl: {
       margin: theme.spacing(1),
       minWidth: 100,
+      marginRight: 10,
     },
     dateFormControl : {
       margin: theme.spacing(1),
